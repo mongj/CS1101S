3ab7ea147975c082df9b220a1c144a1109e806ea
Source Academy User <Ming Jun>
2023-09-18 15:18:54 +0800

    Changes made from Source Academy

--- studios/s6.js
+++ studios/s6.js
@@ -7,7 +7,7 @@ function my_map(f, xs) {
 
 
 // Q2
-function remove_duplicates(lst) {
+function remove_duplicatess(lst) {
     function is_in_list(x, lst) {
         return is_null(lst)
                ? false
@@ -43,4 +43,33 @@ function remove_duplicates(ref) {
 // pred takes in 1 arg (an element in list) and return true or false
 // pred should return true if the element in head does not appear in tail, and false otherwise
 
-remove_duplicates(list(1,2,1,1,3));
+// remove_duplicates(list(1,2,1,1,3));
+
+
+// Q3
+function makeup_amount(x, coins) {
+    if (x === 0) {
+        return list(null);
+    } else if (x < 0 || is_null(coins)) {
+        return null;
+    } else {
+        // Combinations that do not use the head coin.
+        const combi_A = makeup_amount(x, tail(coins));
+
+        // Combinations that do not use the head coin 
+        // for the remaining amount.
+        const combi_B = null;
+
+        // Combinations that use the head coin.
+        const combi_C = map(y => pair(head(coins), y), 
+                            makeup_amount(x - head(coins), tail(coins)));
+                             
+        return append(combi_A, combi_C);
+    }
+}
+
+display_list(makeup_amount(2, list(1, 5, 1, 1)));
+// display_list(makeup_amount(22, list(1, 10, 5, 20, 1, 5, 1, 50)));
+// Result: list(list(20, 1, 1), list(10, 5, 1, 5, 1), list(1, 20, 1),
+//              list(1, 20, 1), list(1, 10, 5, 5, 1), 
+//              list(1, 10, 5, 1, 5))
