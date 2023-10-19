439bc94a309b71bc0e309fcac4ecacd5b1075823
Source Academy User <Ming Jun>
2023-10-19 15:29:15 +0800

    Changes made from Source Academy

--- reflections/r9-q2.js
+++ reflections/r9-q2.js
@@ -1,21 +1,31 @@
 function fib1(n) {
     const a = [0, 1];
-     for (let i = 2; i < n; i = i + 1) {
-         a[i] = a[i - 1] + a[i - 2];
-     }
-     return a[n - 1];
+    for (let i = 2; i <= n; i = i + 1) {
+        a[i] = a[i - 1] + a[i - 2];
+    }
+    return a[n];
 }
 
-display(fib1(8));
+display(fib1(0));
 
 function fib2(n) {
     const a = [0, 1];
-    for (let i = 2; i < n; i = i + 1) {
-        const newfib = a[0] + a[1];
-        a[0] = a[1];
-        a[1] = newfib;
+    for (let i = 2; i <= n; i = i + 1) {
+        a[1] = a[0] + a[1];
+        a[0] = a[1] - a[0];
     }
-    return a[1];
+    return n === 0 ? a[0] : a[1];
 }
 
 display(fib2(8));
+
+
+// 0
+// 1
+// 1
+// 2
+// 3
+// 5
+// 8
+// 13
+// 21
